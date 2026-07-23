527ffdb0cc0c03bba7b4ffe36eebe365686aaad0
Baran Okçuoğlu <baranokcuoglu@Baran-MacBook-Air.local>
2026-07-23 09:37:28 +0300

    wip

--- backend/Entities/Assets.cs
+++ backend/Entities/Assets.cs
@@ -4,14 +4,14 @@ public class Assets
 {
 	public Guid Id { get; set; } = Guid.NewGuid();
     public Guid assetTypeId {get; set; }
-    public AssetTypes AssetType {get; set; }
+    public AssetTypes AssetType { get; set; } = null!;
     public string serialNumber {get; set; } = string.Empty;
     public string brand {get; set; } = string.Empty;
     public string model {get; set; } = string.Empty;
-    public AssetStatus status {get; set; }
+    public AssetAssignment status { get; set; } = null!;
     public DateTime purchaseDate {get; set; }
     public DateTime warrantyEndDate {get; set; }
-    public string location {get; set; }
-    public string notes {get; set; }
+    public string location { get; set; } = string.Empty;
+    public string notes { get; set; } = string.Empty;
 
 }
--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfd5d5a243072cc6cb5678888ee66ae19a8ef867")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a8ee01edba292d8c9c089c32acad485aabeea9")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
