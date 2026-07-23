dcfabac4341ceef64b457a0379074d4b74db0e88
Baran Okçuoğlu <baranokcuoglu@Baran-MacBook-Air.local>
2026-07-23 09:49:29 +0300

    wip

---
+++ backend/Entities/AssetStatus.cs
@@ -0,0 +1,8 @@
+namespace backend.Entites;
+
+public class AssetStatus
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string? ColorCode { get; set; }
+}
