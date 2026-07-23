e329016997a3384449b33fd2eb9c70c9614fc63a
baranokc <baranokcuoglu@gmail.com>
2026-07-23 16:54:19 +0300

    wip

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+876e24aa99b72aa127d99d86a298740590d27f08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6af881517c6bf2e3dfd11de9d1dd56ed429bed9")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
