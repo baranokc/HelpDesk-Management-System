31b0d0eff86376ba9f1cffa21a2a805463c3f6c7
MFistikci <mesutcanfistikci13@gmail.com>
2026-07-30 10:46:01 +0300

    frontend wip

---
+++ frontend/src/services/lookupService.ts
@@ -0,0 +1,24 @@
+import { api } from "@/src/lib/api";
+import { LookupItemDto, TeamMemberLookupDto } from "@/src/types/common"
+import { get } from "http";
+
+async function getLookup<TId = string> (path:string) {
+    const response = await api.get<LookupItemDto<TId>[]>(path);
+    return response.data;
+}
+
+export const lookupService = {
+    getCategories: () => getLookup("lookups/categories"),
+    getSubcategories: (categoryId: string) => getLookup ('lookups/categories/${categoryId}/subcategories'),
+    getPriorities: () => getLookup("lookups/priorities"),
+    getStatuses: () => getLookup("lookups/statuses"),
+    getImpactLevels: () => getLookup("lookups/impact-levels"),
+    getUrgencyLevels: () => getLookup("lookups/urgency-levels"),
+    getTeams: () => getLookup("lookups/teams"),
+    getDepartments: () => getLookup<number>("lookups/departments"),
+    getRoles: () => getLookup("lookups/roles"),
+    getResolutionCategories: () => getLookup("lookups/resolution-categories"),
+    getTeamMembers: async (teamId: string): Promise<TeamMemberLookupDto[]> => {const response = await api.get<TeamMemberLookupDto[]>('lookups/teams/${teamId}/members');
+        return response.data;
+    }
+}
---
+++ frontend/src/types/common.ts
@@ -0,0 +1,20 @@
+export interface PagedResultDto<T> {
+    items: T[];
+    pageNumber: number;
+    pageSize: number;
+    totalCount: number;
+    hasPreviousPage: boolean;
+    hasNextPage: boolean;
+}
+
+export interface LookupItemDto <TId=string> {
+    itemId: TId;
+    name: string;
+}
+
+export interface TeamMemberLookupDto {
+    teamMemberId: string;
+    userId: string;
+    fullName: string;
+    roleInTeam: string;
+}
